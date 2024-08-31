87e84cc6c6db43fa4dacdf675b586f01f8cf290a
Araya4 <araya.wut@gmail.com>
2024-08-31 11:22:22 +0700

    Update code.js

--- code.js
+++ code.js
@@ -1,13 +1,9 @@
 const myVideo = document.getElementById("myVideo");
 
-const videoID = ["k3oleRNLy7J0aQBnv9u","k7DZP8PohA6i2WBnv9C","","","","","k1qU19sAOu7wchBnv9A","k2ELflla5ecHfxBnv9E",
+const videoID = ["k3oleRNLy7J0aQBnv9u","k7DZP8PohA6i2WBnv9C","k6VqvAAvot69BQBnSEE","k2DD9MLRWKj5yzBnSEK","k3pDJQcIIvCP7RBnSEI","k3mLQDnCUHZyY7BnSEG","k1qU19sAOu7wchBnv9A","k2ELflla5ecHfxBnv9E",
 "k3xfBkrk2K6usbBnv9G","k13eA7jTxjjT9iBnv9I","k6QS0CDEsOKVhlBnv9K"];
 const videoUrl = videoID.map(v=>{
-    if(v==="") { 
-        return "about:blank";
-    }else{ 
-        return `https://www.dailymotion.com/embed/video/${v}?autoplay=1`;
-    }
+    return `https://www.dailymotion.com/embed/video/${v}?autoplay=1`;
 })
 
 
